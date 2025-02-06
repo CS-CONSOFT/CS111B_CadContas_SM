472cff8406c917cf694adaf7a3430445fda1ecef
Victor Santos <victor.manoel@cs-consoft.com.br>
2025-02-06 15:43:54 -0300

    Update
    Atualização das rotas de acesso as APIs e submódulos.

--- src/services/configuracoes_axios.ts
+++ src/services/configuracoes_axios.ts
@@ -1,5 +1,5 @@
-const URLBase = import.meta.env.VITE_API_URL + '/';
-const newURLBase = 'https://open1.heei.com.br';
-const URL_PDVAPI = URLBase + 'cs_At_40_LogicoService/rest/CS_PDV_API/';
+const URLBase = import.meta.env.VITE_API_URL;
+const newURLBase = import.meta.env.VITE_API_URL;
+const URL_PDVAPI = import.meta.env.VITE_URL_BASEOS + '/cs_At_40_LogicoService/rest/CS_PDV_API/';
 
-export { URLBase, URL_PDVAPI, newURLBase };
+export { URLBase, newURLBase, URL_PDVAPI };
